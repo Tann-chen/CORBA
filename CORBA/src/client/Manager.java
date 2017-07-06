fe473c3b036e06cf7220d7f01b2ec81677b39428
Grey <GreyLee369@gmail.com>
2017-07-06 16:39:28 -0400

    "Client CORBA connection"

--- CORBA/src/client/Manager.java
+++ CORBA/src/client/Manager.java
@@ -1,43 +1,38 @@
 package client;
 
-
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.util.Date;
-
-
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
+import DCMS.CenterServer;
+import DCMS.CenterServerHelper;
 
 
 public class Manager {
 	private String managerID;
 	private String name;
-	private Registry registry;
-
-	private static File loggingFile=new File("Manager.txt");
+	private CenterServer centerServerImp;
+	private static File loggingFile = new File("Manager.txt");
 
 	public Manager(String managerID, String name, String[] args){
 		this.managerID = managerID;
 		this.name = name;
-		//distributing server
 
-		String nameService;
+
+		String nameContext;
 		if(this.managerID.startsWith("MTL")){
-			nameService="MTL";
+			nameContext="MTL";
 		}
 		else if(this.managerID.startsWith("LVL")){
-			nameService="LVL";
+			nameContext="LVL";
 		}
 		else if(this.managerID.startsWith("DDO")){
-			nameService="DDO";
+			nameContext="DDO";
 		}
 		else{
 			System.out.println("Error:invalid managerID");
@@ -49,62 +44,45 @@ public class Manager {
 	        ORB orb = ORB.init(args, null);
 	         // get the root naming context
 	        org.omg.CORBA.Object objRef =
-	            orb.resolve_initial_references(nameService);
+	            orb.resolve_initial_references("NameService");
 	        // Use NamingContextExt instead of NamingContext. This is
 	        // part of the Interoperable naming Service.
 	        NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 	        // resolve the Object Reference in Naming
-	        String message = "Hello";
-	        helloImpl = HelloHelper.narrow(ncRef.resolve_str(message));
-	         System.out.println("Obtained a handle on server object: " + helloImpl);
-	        System.out.println(helloImpl.sayHello());
-//	        helloImpl.shutdown();
+
+	        centerServerImp = CenterServerHelper.narrow(ncRef.resolve_str(nameContext));
+	         System.out.println("Obtained a handle on server object: " + centerServerImp);
+//	        System.out.println(centerServerImp.sayHello());
 	         } catch (Exception e) {
 	          System.out.println("ERROR : " + e) ;
 	          e.printStackTrace(System.out);
 	          }
 	}
 
-	public void createTRecord(String firstName, String lastName, String address, String phone, String specialization, String location){
+	public void createTRecord(String managerId, String firstName, String lastName, String address, String phone, String specialization, String location){
 		boolean flag=false;
-		try {
-			flag=centerServer.createTRecord(firstName, lastName, address, phone, specialization, location);
-		}catch (RemoteException re){
-			re.getStackTrace();
-		}
+		flag=centerServerImp.createTRecord(managerId,firstName, lastName, address, phone, specialization, location);
 		String log=(new Date().toString())+" | "+this.name + "- create teacher record - "+ String.valueOf(flag);
 		writelog(log);
 	}
 
-	public void createSRecord(String firstName, String lastName, String coursesRegistered, String status, String date){
+	public void createSRecord(String managerId, String firstName, String lastName, String coursesRegistered, String status, String date){
 		boolean flag=false;
-		try{
-			flag=centerServer.createSRecord(firstName, lastName, coursesRegistered, status, date);
-		}catch (RemoteException re) {
-			re.getStackTrace();
-		}
+		flag=centerServerImp.createSRecord(managerId,firstName, lastName, coursesRegistered, status, date);
 		String log=(new Date().toString())+" | "+this.name + "- create student record - "+ flag;
 		writelog(log);
 	}
 
-	public void getRecordCounts() throws RemoteException {
+	public void getRecordCounts(String managerId) throws RemoteException {
 			String result=null;
-		try{
-			result=centerServer.getRecordCounts();
-		}catch (RemoteException re){
-			re.getStackTrace();
-		}
+		result=centerServerImp.getRecordCounts(managerId);
 		String log=(new Date().toString())+" | "+this.name + "- get records count - "+ result;
 		writelog(log);
 		System.out.println(result);
 	}
 
-	public void editRecord(String recordID, String fieldName, String newValue) throws RemoteException {
-		try {
-			centerServer.editRecord(recordID, fieldName, newValue);
-		}catch (RemoteException re){
-			re.getStackTrace();
-		}
+	public void editRecord(String managerId,String recordID, String fieldName, String newValue) throws RemoteException {
+		centerServerImp.editRecord(managerId,recordID, fieldName, newValue);
 		String log=(new Date().toString())+" | "+this.name + "- edit record - ";
 		writelog(log);
 	}
--- CORBA/src/client/ManagerClient.java
+++ CORBA/src/client/ManagerClient.java
@@ -6,43 +6,43 @@ public class ManagerClient {
 	public static void main(String[] args) throws Exception {
 
 		Manager manager1=new Manager("MTL1111","manager1",args);
-		manager1.createSRecord("chen1","A","Math","active","2008-10-02");
-		manager1.getRecordCounts();
+		manager1.createSRecord("MTL1111","chen1","A","Math","active","2008-10-02");
+		manager1.getRecordCounts("MTL1111");
 
 		Manager manager2=new Manager("DDO1111","manager2",args);
-		manager2.createSRecord("chen2","B","English","active","2008-10-03");
+		manager2.createSRecord("DDO1111","chen2","B","English","active","2008-10-03");
 
 		Manager manager3=new Manager("LVL1111","manager3",args);
-		manager3.createSRecord("chen3","C","French","active","2008-10-04");
-		manager3.getRecordCounts();
+		manager3.createSRecord("LVL1111","chen3","C","French","active","2008-10-04");
+		manager3.getRecordCounts("LVL1111");
 
 		Manager manager4=new Manager("MTL2222","manager4",args);
-		manager4.createTRecord("li4","D","Tupper street","123456789","Math","LVL");
-		manager4.createSRecord("li5","E","math","active","2016-09-01");
+		manager4.createTRecord("MTL2222","li4","D","Tupper street","123456789","Math","LVL");
+		manager4.createSRecord("MTL2222","li5","E","math","active","2016-09-01");
 
 		Manager manager5=new Manager("DDO2222","manager5",args);
-		manager5.createTRecord("li6","F","Du fort","987654321","English","MTL");
-		manager5.getRecordCounts();
+		manager5.createTRecord("DDO2222","li6","F","Du fort","987654321","English","MTL");
+		manager5.getRecordCounts("DDO2222");
 		
 		Manager manager6=new Manager("LVL2222","manager6",args);
-		manager6.editRecord("SR100", "coursesRegistered", "Spanish");
+		manager6.editRecord("LVL2222","SR100", "coursesRegistered", "Spanish");
 		
-		manager1.createTRecord("li7","G","Guy","1357924680","Chinese","MTL");
-		manager1.getRecordCounts();
+		manager1.createTRecord("MTL1111","li7","G","Guy","1357924680","Chinese","MTL");
+		manager1.getRecordCounts("MTL1111");
 
 		Manager manager7=new Manager("MTL3333","manager7",args);
-		manager7.editRecord("SR10001", "coursesRegistered", "english");
-		manager7.createTRecord("lli","xi","mtl","12121","xxx","mtl");
-		manager7.getRecordCounts();
+		manager7.editRecord("MTL3333","SR10001", "coursesRegistered", "english");
+		manager7.createTRecord("MTL3333","lli","xi","mtl","12121","xxx","mtl");
+		manager7.getRecordCounts("MTL3333");
 
 
 		Manager manager8=new Manager("DDO3333","manager8",args);
-		manager8.editRecord("TR10001", "phone", "1239876540");
-		manager8.getRecordCounts();
-		manager8.createSRecord("ti","yi","math","active","2018-01-01");
-		manager8.getRecordCounts();
+		manager8.editRecord("DDO3333","TR10001", "phone", "1239876540");
+		manager8.getRecordCounts("DDO3333");
+		manager8.createSRecord("DDO3333","ti","yi","math","active","2018-01-01");
+		manager8.getRecordCounts("DDO3333");
 		
-		manager2.createSRecord("chen2","B","English","active","2008-10-03");
-		manager2.getRecordCounts();
+		manager2.createSRecord("DDO1111","chen2","B","English","active","2008-10-03");
+		manager2.getRecordCounts("DDO1111");
 	}
 }
