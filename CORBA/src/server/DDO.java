3b917c64eb265b77aa32eccb5357043f8f1b0abd
Tann <Tianen.chen@outlook.com>
2017-07-06 16:48:21 -0400

    server CORBA

--- CORBA/src/server/DDO.java
+++ CORBA/src/server/DDO.java
@@ -17,52 +17,48 @@ import DCMS.CenterServerHelper;
 
 
 public class DDO {
-	private static CenterServer centerServer;
+	private static CenterServerImp centerServerSurvant;
 
-	public static void main(String[] args) throws Exception {
-//		File logFile = new File("ddo.txt");
-//		centerServer= new CenterServerImp(logFile,"DDO");
-//
-//		Registry registry = LocateRegistry.createRegistry(3002);
-//		registry.bind("DDOCenter", centerServer);
-//		System.out.println("DDO start");
-		
+	public static void main(String[] args){
+
 		try{
-		      // create and initialize the ORB
-		      ORB orb = ORB.init(args, null);
-		      // get reference to rootpoa & activate the POAManager
-		      POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-		      rootpoa.the_POAManager().activate();
-		      // create servant and register it with the ORB
-		      HelloImpl helloImpl = new HelloImpl();
-		      helloImpl.setORB(orb);
-		      // get object reference from the servant
-		      org.omg.CORBA.Object ref = rootpoa.servant_to_reference(helloImpl);
-		      CenterServer href = CenterServerHelper.narrow(ref);
-		      // get the root naming context
-		      // NameService invokes the name service
-		      org.omg.CORBA.Object objRef =
-		          orb.resolve_initial_references("DDO");
-		      // Use NamingContextExt which is part of the Interoperable
-		      // Naming Service (INS) specification.
-		      NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+			File logFile = new File("ddo.txt");
 
-		    // bind the Object Reference in Naming
-		      String name = "Hello";
-		      NameComponent path[] = ncRef.to_name( name );
-		      ncRef.rebind(path, href);
-		         System.out.println("CenterServer ready and waiting ...");
-		        // wait for invocations from clients
-		      orb.run();
-		    } 
-		        catch (Exception e) {
-		        System.err.println("ERROR: " + e);
-		        e.printStackTrace(System.out);
-		      }
-		          System.out.println("CenterServer Exiting ...");
-		        
+			// create and initialize the ORB
+			ORB orb = ORB.init(args, null);
+			// get reference to rootpoa & activate the POAManager
+			POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+			rootpoa.the_POAManager().activate();
+			// create servant and register it with the ORB
+			centerServerSurvant= new CenterServerImp(logFile,"DDO");
+			centerServerSurvant.setORB(orb);
+			// get object reference from the servant
+			org.omg.CORBA.Object ref = rootpoa.servant_to_reference(centerServerSurvant);
+			CenterServer href = CenterServerHelper.narrow(ref);
+			// get the root naming context
+			// NameService invokes the name service
+			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+			// Use NamingContextExt which is part of the Interoperable
+			// Naming Service (INS) specification.
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+			// bind the Object Reference in Naming
+			String name = "DDO";
+			NameComponent path[] = ncRef.to_name(name);
+			ncRef.rebind(path, href);
+			System.out.println("DDOServer ready and waiting ...");
+			// wait for invocations from clients
+			orb.run();
+		}
+		catch (Exception e) {
+			System.err.println("ERROR: " + e);
+			e.printStackTrace(System.out);
+		}
 
-	
+
+		/*
+		Registry registry = LocateRegistry.createRegistry(3002);
+		registry.bind("DDOCenter", centerServer);
+		System.out.println("DDO start");
 		//listening to request
 		DatagramSocket datagramSocket = null;
 		try {
@@ -76,14 +72,13 @@ public class DDO {
 				datagramSocket.receive(request);
 				new MyThread(request.getAddress(),request.getPort(),datagramSocket,centerServer).start();
 			}
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}finally {
 			if(datagramSocket != null)
 				datagramSocket.close();
 		}
-		
-		
+		*/
 	}
 }
