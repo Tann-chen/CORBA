847d94ed08692ad2875305fd0e7f575811a00729
Tann <Tianen.chen@outlook.com>
2017-07-09 10:37:18 -0400

    client multithread

--- CORBA/src/client/ManagerClient.java
+++ CORBA/src/client/ManagerClient.java
@@ -9,7 +9,7 @@ public class ManagerClient {
 
 
 	public static void main(String[] args){
-		System.out.print("running");
+
 		try{
 			ORB orb = ORB.init(args, null);
 			org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
@@ -21,19 +21,21 @@ public class ManagerClient {
 			e.printStackTrace(System.out);
 		}
 
+		new ClientThread().start();
+
 		Manager manager1=new Manager("MTL1111");
 		manager1.createSRecord("chen1","A","Math","active","2008-10-02");
 		manager1.getRecordCounts();
+		System.out.println("manager1 running");
 
 		Manager manager2=new Manager("DDO1111");
 		manager2.createSRecord("chen2","B","English","active","2008-10-03");
+		System.out.println("manager2 running");
 
 		Manager manager3=new Manager("LVL1111");
 		manager3.createSRecord("chen3","C","French","active","2008-10-04");
 		manager3.getRecordCounts();
+		System.out.println("manager3 running");
 
-		manager1.shutdown();
-		manager2.shutdown();
-		manager3.shutdown();
 	}
 }
